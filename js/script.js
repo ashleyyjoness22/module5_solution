c5b49bd54f24cce3456feb353d20ea97e9b63ded
Ashley Jones <jonesa0996@gmail.com>
2023-04-07 14:12:42 -0500

    bs

--- js/script.js
+++ js/script.js
@@ -117,9 +117,8 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 // it into the home html snippet.
                 //
                 // var homeHtmlToInsertIntoMainPage = ....
-
-            chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
-            var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+            
+            var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
 
                 // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
                 // Use the existing insertHtml function for that purpose. Look through this code for an example
